56cdd1b6ef12ceeab0af7f67e37bffb4568d14b7
Staypcy <2174738782@qq.com>
2026-09-07 11:35:20 +0800

    add CMake

---
+++ vidflow/main.cpp
@@ -0,0 +1,14 @@
+#include"threadpool.h"
+#include<iostream>
+#include<cstddef>
+#include<cstdint>
+#include<stdexcept>
+
+int main(int argc , char** argv){
+    if(argc!=2){
+        std::cerr<<"Usage: ./test_lock count\n";
+        return -1;
+    }
+
+    
+}
